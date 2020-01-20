e4ebec7616ee68161c7ffffcb604381b74f96f50
Bargate <837014@cognizant.com>
2020-01-20 17:21:42 +0530

    first commit

---
+++ CI.java
@@ -0,0 +1,18 @@
+     class CI
+	{  
+    
+	    public static void main(String args[])
+	      {  
+     
+		System.out.println("This is line 1");  
+       			
+		System.out.println("This is line 2");  
+        
+		System.out.println("This is line 3");  
+        
+		System.out.println("This is line 4");  
+       
+ 
+    	      }  
+	
+}  
